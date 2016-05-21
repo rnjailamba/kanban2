130506cfe6ddce0a8589a2dfc5936872eab2f8e6
rnjai <rnjailamba12@gmail.com>
2016-05-21 07:19:13 +0530

    Socket demo finished

--- views/kanban/kanbanAngular/app/main/apps/scrumboard/services/socket.service.js
+++ views/kanban/kanbanAngular/app/main/apps/scrumboard/services/socket.service.js
@@ -1,17 +1,3 @@
-// app.factory('socket', ['$rootScope', function($rootScope) {
-//   var socket = io.connect();
-//
-//   return {
-//     on: function(eventName, callback){
-//       socket.on(eventName, callback);
-//     },
-//     emit: function(eventName, data) {
-//       socket.emit(eventName, data);
-//     }
-//   };
-// }]);
-
-
 (function ()
 {
     'use strict';
